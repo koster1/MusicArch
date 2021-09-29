550f46770ded1043002cf4e0b9878da142eb1de6
alexanss <alexander.sanmiguel@metropolia.fi>
2021-09-29 15:36:17 +0300

    Chad saved us! - Alex

--- src/main/java/com/jcg/hibernate/maven/Genre.java
+++ src/main/java/com/jcg/hibernate/maven/Genre.java
@@ -1,9 +1,14 @@
 package com.jcg.hibernate.maven;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,18 +46,30 @@ public class Genre implements Serializable {
 	
 	
 	//Everything from down here is highly experimental
-	@ManyToMany(
-			targetEntity=com.jcg.hibernate.maven.Album.class,
-			cascade= {CascadeType.PERSIST, CascadeType.MERGE}
-			)
+	@ManyToMany(fetch=FetchType.LAZY,
+			cascade = {CascadeType.ALL})
 	@JoinTable(
 			name="koostuu",
 			joinColumns={@JoinColumn(name="GenreID")},
-			inverseJoinColumns= {@JoinColumn(name="AlbumiID")}
+			inverseJoinColumns={@JoinColumn(name="AlbumiID")}
 			)
-	public Collection getAlbums() {
-		return albums;
+	private List<Album> genreAlbums;
+	
+	public List<Album> getGenreAlbums(){
+		return genreAlbums;
 	}
+	
+	public void setGenreAlbums(List<Album> genreAlbums) {
+		this.genreAlbums = genreAlbums;
+	}
+	public void addAlbum(Album album) {
+		if(genreAlbums == null) {
+			genreAlbums = new ArrayList<>();
+		}
+		genreAlbums.add(album);
+	}
+	
+	public Genre() {}
 	
 	
 }
