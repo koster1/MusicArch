33979f3ef537beb2e7bdda1cbc6b299f737812fd
jemmmat <55233574+jemmmat@users.noreply.github.com>
2021-10-01 15:17:40 +0300

    Viimeisin versio käyttöliittymästä.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,28 +1,12 @@
 package main;
 
-import java.util.*;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-import com.jcg.hibernate.maven.RemoteDAO;
-import com.jcg.hibernate.maven.Album;
-import com.jcg.hibernate.maven.Artist;
-import com.jcg.hibernate.maven.Genre;
-import com.jcg.hibernate.maven.LocalDAO;
-
-import controller.Controller;
-import model.DeletionManagement;
-import model.DeliveryObject;
-import model.InputManagement;
-import model.SearchManagement;
 import view.*;
 
 public class Main {
 	
 	public static void main(String args[]) {
-
+		System.out.println("HELLO :)");
 		View.main(args);
 
 
