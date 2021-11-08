a0091d1e1fbbfd257c05ab20f92f94cba0e730b7
kalleos <kalleos@metropolia.fi>
2021-11-08 11:03:47 +0200

    helpPagen tiedoston lukemisen tekoa

--- src/main/java/controller/HelpController.java
+++ src/main/java/controller/HelpController.java
@@ -1,6 +1,13 @@
 package controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -12,11 +19,28 @@ public class HelpController {
 	private AnchorPane helpPage;
 	
 	@FXML
-	private Text helptext;
+	private Label helptext;
+	
 	
 	@FXML
-	protected void initialize() {
-
+	 protected void initialize() { 
 	}
+	 
+	public static void main(String[] args) throws IOException {
+		
+		try {
+			File file = new File("/src/main/resources/helpfin.txt");
+			Scanner myReader = new Scanner(file);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();					
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch(FileNotFoundException e) {
+		    throw new FileNotFoundException("HelpPage not found");
+		}
+		
+	}
+	
 }
 
